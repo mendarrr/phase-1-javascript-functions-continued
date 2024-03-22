0f18266d00846bec5bc6f57dddfa14e5840142d3
mendarrr <sarahabby541@gmail.com>
2024-03-22 21:21:42 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+      activity = "roller-skate";
+    }
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  console.log(saturdayFun("bathe my dog"));
+  console.log(saturdayFun());
+  
+  function mondayWork(activity) {
+    if (activity === undefined) {
+      activity = "go to the office";
+    }
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  console.log(saturdayFun("work from home"));
+  console.log(saturdayFun());
+  
+  function wrapAdjective(visualFlair = "*") {
+    return function(adj = "special") {
+      return `You are ${visualFlair}${adj}${visualFlair}!`;
+    };
+  }
+  
+  const encouragingPromptFunction = wrapAdjective("!!!")
+  console.log(encouragingPromptFunction("a dedicated programmer"));
